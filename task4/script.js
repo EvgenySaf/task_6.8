f01e50b08fe5eada86ac8bfc3caa68e4f5d0a47a
EvgenySaf <esminx@mail.ru>
2022-11-08 16:53:10 +0300

    init

---
+++ task4/script.js
@@ -0,0 +1,6 @@
+const currentUrl = document.querySelector('#currentUrl');
+currentUrl.addEventListener('click', (event) => {
+    const newUrl = prompt('Введите новый текст для ссылки');
+    currentUrl.textContent = newUrl;
+    event.preventDefault()
+})
